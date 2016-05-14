e75b92e1c42316de9ccd2d7c6e6a5779e2b7b11f
“jhondominguez1” <jjjdomi@gmail.com>
2016-05-14 15:42:59 -0500

    En este archivo actulice dos lineas

---
+++ index3.php
@@ -0,0 +1,10 @@
+ El volumen de la unidad D es Particion_1
+ El n£mero de serie del volumen es: 0AAC-AFC3
+
+ Directorio de D:\local_softwareprueba
+
+14/05/2016  03:39 p. m.    <DIR>          .
+14/05/2016  03:39 p. m.    <DIR>          ..
+14/05/2016  03:39 p. m.                 0 index3.php
+               1 archivos              0 bytes
+               2 dirs  76.760.993.792 bytes libres
